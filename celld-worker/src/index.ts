ba3570038fb166e441464754c293ee1dfb5e6860
xpakx <55586470+xpakx@users.noreply.github.com>
2026-08-28 21:46:55 +0200

    feat: use Durable Object for chat

--- celld-worker/src/index.ts
+++ celld-worker/src/index.ts
@@ -1,28 +1,52 @@
+import { DurableObject } from "cloudflare:workers";
+
+export interface Env {
+	CHAT_ROOM: DurableObjectNamespace;
+}
+
 export default {
-	async fetch(request: Request, _env: Env, _ctx: ExecutionContext): Promise<Response> {
+	async fetch(request: Request, env: Env, _ctx: ExecutionContext): Promise<Response> {
 		const url = new URL(request.url);
 
 		if (url.pathname === "/ws") {
-			const upgradeHeader = request.headers.get("Upgrade");
-			if (upgradeHeader !== "websocket") {
-				return new Response("Expected Upgrade: websocket", { status: 426 });
-			}
+			const id = env.CHAT_ROOM.idFromName("global-chat");
+			const stub = env.CHAT_ROOM.get(id);
+			return stub.fetch(request);
+		}
+		return new Response("Not found", { status: 404 });
+	}
+} satisfies ExportedHandler<Env>;
 
-			const pair = new WebSocketPair();
-			const [client, server] = Object.values(pair);
-
-			server.accept();
-
-			server.addEventListener("message", (event) => {
-				server.send(`Echo: ${event.data}`);
-			});
-
-			return new Response(null, {
-				status: 101,
-				webSocket: client,
-			});
+export class ChatRoom extends DurableObject {
+	async fetch(request: Request): Promise<Response> {
+		const upgradeHeader = request.headers.get("Upgrade");
+		if (upgradeHeader !== "websocket") {
+			return new Response("Expected Upgrade: websocket", { status: 426 });
 		}
 
-		return new Response("Not found", { status: 404 });
-	},
-} satisfies ExportedHandler<Env>;
+		const pair = new WebSocketPair();
+		const [client, server] = Object.values(pair);
+
+		this.ctx.acceptWebSocket(server);
+
+		return new Response(null, {
+			status: 101,
+			webSocket: client,
+		});
+	}
+
+	async webSocketMessage(ws: WebSocket, message: string | ArrayBuffer): Promise<void> {
+	   const sockets = this.ctx.getWebSockets();
+	   for (const socket of sockets) {
+		   if (socket == ws) {
+			   socket.send("Accepted: " + message);
+			   continue;
+		   }
+		   socket.send(message);
+	   }
+	}
+
+	async webSocketClose(ws: WebSocket, code: number, reason: string, _wasClean: boolean): Promise<void> {
+		ws.close(code, reason);
+	}
+}

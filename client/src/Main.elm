2106130144909bf55c687d48f511d79993dd18a7
xpakx <55586470+xpakx@users.noreply.github.com>
2026-08-28 22:48:50 +0200

    feat: make basic chat logic in frontend

--- client/src/Main.elm
+++ client/src/Main.elm
@@ -1,11 +1,55 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (text)
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
 
-main =
-    Browser.sandbox {
-            init = (),
-            update = \_ model -> model,
-            view = \_ -> text "Hello, Elm!"
-    }
+type alias Model = {
+        inputMsg : String,
+        msgs : List String
+        }
+
+type Msg
+    = OnClick
+    | OnInput String
+
+view : Model -> Html Msg
+view model =
+    div [ class "chat" ]
+        [ h1 [] [ text "Chat" ]
+        , p []
+            [ 
+                    text "Disconnected",
+                    div [] 
+                    (List.map (\msg -> Html.li [] [ text msg ]) model.msgs)
+                    ,
+                    input [ placeholder "Type a message...",
+                    value model.inputMsg,
+                    onInput OnInput ] [],
+                    button [ onClick OnClick] [ text "Send" ]
+            ]
+        ]
+
+update : Msg -> Model -> Model
+update msg model =
+        case msg of 
+                OnInput newMsg -> 
+                        { model | inputMsg = newMsg }
+                OnClick ->
+                        { model 
+                        | msgs = model.msgs ++ [ model.inputMsg ],
+                        inputMsg = ""
+                        }
+
+initialModel : Model
+initialModel = {
+        inputMsg = "",
+        msgs = []
+        }
+
+main = Browser.sandbox {
+        init = initialModel,
+        view = view,
+        update = update
+        }
